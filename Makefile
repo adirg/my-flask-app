f6f66cc4c0578e39f37cdebdc24077ddcc9c0230
Adir Gabai <adirgabai@gmail.com>
2017-03-21 09:11:42 +0200

    Initial commit.

---
+++ Makefile
@@ -0,0 +1,15 @@
+PACKAGE=my_flask_app
+
+all: flake8 pylint sdist
+
+sdist:
+	python setup.py sdist
+
+pylint:
+	PYLINTHOME=./build pylint -r n $(PACKAGE)
+
+flake8:
+	flake8 $(PACKAGE)
+
+clean:
+	rm -rf build dist *.egg-info
---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+import setuptools
+
+
+setuptools.setup(
+    name='my-flask-app',
+    version='1.0.0',
+    author='Adir Gabai',
+    author_email='adirgabai@gmail.com',
+    packages=['my_flask_app'],
+)
